fec95bb3c8aeb5deef0cd1f9c9091f50e3b60b70
Elijah Glascock <ejglascock@gmail.com>
2022-06-22 11:42:46 -0700

    display has functionality to display name and types of pokemon search result

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -1,6 +1,17 @@
 import React, {Fragment, useState} from "react";
 
 const Display = (props) => {
+
+    if (props.pokemon.id) {
+        return (
+            <Fragment>
+                <h1 className="text-center mt-5">{props.pokemon.name}</h1>
+                {props.pokemon.types.map(e => (
+                    <h2 className="text-center mt-2">{e.type.name}</h2>
+                ))}
+            </Fragment>
+        )
+    }
 }
 
 
