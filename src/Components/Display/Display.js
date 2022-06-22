817f5a3b52f4da646191025644f1ff2884d341ba
Elijah Glascock <ejglascock@gmail.com>
2022-06-22 13:23:32 -0700

    centered img and added some shadow effect

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -1,11 +1,12 @@
 import React, {Fragment, useState} from "react";
+import './Display.css';
 
 const Display = (props) => {
 
     if (props.pokemon.id) {
         return (
             <Fragment>
-                <img className="pokemon" src={props.pokemon.sprites.other["official-artwork"]["front_default"]} ></img>
+                <img class="center" src={props.pokemon.sprites.other["official-artwork"]["front_default"]} ></img>
                 <h1 className="text-center mt-5">{props.pokemon.name}</h1>
                 {props.pokemon.types.map(e => (
                     <h2 className="text-center mt-2" key={e.slot}>{e.type.name}</h2>
