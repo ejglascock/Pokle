67b97d7f4e3535d4fcac8544e7e191eddd128eb7
ejglascock <ejglascock@gmail.com>
2022-09-22 16:56:11 -0700

    fixed issue where letters weren't showing correctly that they were in the right spot or the right letter

--- src/Components/Letter/Letter.js
+++ src/Components/Letter/Letter.js
@@ -5,8 +5,8 @@ function Letter({ letterPos, attemptVal }) {
     const { board, pokeOfDay, currAttempt, wrongLetters, setWrongLetters, gameOver } = useContext(AppContext);
     const letter = board[attemptVal][letterPos];
 
-    const correct = pokeOfDay[letterPos] === letter
-    const almost = !correct && letter !== "" && pokeOfDay.includes(letter);
+    const correct = pokeOfDay[letterPos] === letter.toLowerCase()
+    const almost = !correct && letter.toLowerCase() !== "" && pokeOfDay.includes(letter.toLowerCase());
 
     const letterState = currAttempt.attempt > attemptVal && (correct ? "correct" : almost ? "almost" : "error");
 
