3897a44f6e3767d06012b73c72a1900e3afc8c3e
ejglascock <ejglascock@gmail.com>
2022-09-28 15:53:58 -0700

    at game over screen the pokemon name should now show up in all capitals

--- src/Components/GameOver/GameOver.js
+++ src/Components/GameOver/GameOver.js
@@ -6,7 +6,7 @@ function GameOver() {
     return (
         <div className="gameOver">
             <h3>{gameOver.guessedWord ? "You guessed correctly!" : "You were not able to guess correctly"}</h3>
-            <h1>Correct Pokemon: {pokeOfDay}</h1>
+            <h1>Correct Pokemon: {pokeOfDay.toUpperCase()}</h1>
             {gameOver.guessedWord && (<h3>You guessed in {currAttempt.attempt} attempts.</h3>)}
         </div>
     )
